fe36d094ddd484b0b6d33fe2f090b0bf3d515042
천태웅 <ctw0727@naver.com>
2021-04-15 01:16:25 +0900

    관절 구현 및 테스트 완료된 버전
    기본적인 관절 구현 끝

--- Box_Smasher/Assets/Scripts/Precoding/RoboticHandPrecoding.cs
+++ Box_Smasher/Assets/Scripts/Precoding/RoboticHandPrecoding.cs
@@ -14,10 +14,18 @@ using UnityEngine;
 
 public class RoboticHandPrecoding : MonoBehaviour
 {
-    private Vector3 PosStart, PosAnkle, PosDef;		// 시작지점, 관절위치, 목표지점
-	private float StoA, AtoD;						// 관절의 길이
+	private Vector3 MouseStaticPos;				// 마우스의 위치를 받아오기 위한 Vector3
+	private Transform Root, Main, Last;			// Scene 안의 실제 시작지점, 관절위치, 목표지점 (Control용도)
+    private Vector3 PosStart, PosDef;			// 실제 위치에서 받아온 가상의 Vector3 시작지점, 목표지점
+	private Vector3 PosAnkle;					// 계산된 가상의 관절 위치를 저장하는 Vector3
+	private Vector3 PosAnkle_last;				// 마지막으로 계산된 가상의 관절 위치를 저장하는 Vector3
+	private bool Lefted;						// 관절이 어느쪽으로 휠지 정하는 bool
+	public float StoA, AtoD;					// 관절의 길이를 정하기 위한 float
+	public Camera MainCamera;					// 마우스 위치를 받아오기 위한 카메라
 	
-	// Pos1에서 Pos2로 향하는 각도를 반환
+	/// <summary>
+	/// Pos1에서 Pos2로 향하는 각도를 반환
+	/// </summary>
 	private Quaternion Get_Pos_rotation(Vector3 Pos1, Vector3 Pos2){
         
         float angle = Mathf.Atan2(Pos2.y-Pos1.y, Pos2.x-Pos1.x) * Mathf.Rad2Deg;
@@ -27,68 +35,124 @@ public class RoboticHandPrecoding : MonoBehaviour
         return rotation;
     }
 	
-	// 입력받은 두 좌표간의 거리를 반환
-    float Get_Vector3_Range(Vector3 Pos1, Vector3 Pos2){
+	/// <summary>
+	/// 입력받은 두 좌표간의 거리를 반환
+	/// </summary>
+    private float Get_Vector3_Range(Vector3 Pos1, Vector3 Pos2){
 		
         return Mathf.Sqrt(Mathf.Pow((Pos1.x - Pos2.x),2)+Mathf.Pow((Pos1.y - Pos2.y),2));
     }
 	
-	// PosAnkle의 좌표를 구하는 함수
-	private Vector3 Get_Pos_Ankle(Vector3 P1, Vector3 P2, bool Lefted){
+	/// <summary>
+	/// 두 좌표의 중간지점의 좌표를 반환
+	/// </summary>
+	private Vector3 Get_Vector3_Middle(Vector3 P1, Vector3 P2){
+		return new Vector3(P2.x + (P1.x - P2.x)/2, P2.y + (P1.y - P2.y)/2, 0);
+	}
+	
+	/// <summary>
+	/// PosAnkle의 좌표를 구하는 함수
+	/// </summary>
+	private Vector3 Get_Pos_Ankle(Vector3 P1, Vector3 P2){
 		
 		float Length = Get_Vector3_Range(P1, P2);
-		float buf1 = Mathf.Acos(( Mathf.Pow(StoA, 2) - Mathf.Pow(AtoD, 2) + Mathf.Pow(Length, 2) ) / ( 2 * StoA * Length));
-		float buf2 = Mathf.Atan( (P2.y - P1.y) / (P2.x - P1.x) );
+		float buf1, buf2, buf3, buf4;
+		buf1 = Mathf.Acos(( Mathf.Pow(StoA, 2) - Mathf.Pow(AtoD, 2) + Mathf.Pow(Length, 2) ) / ( 2 * StoA * Length));
 		
 		if (Lefted == false){
-			float buf3 = P1.x + StoA * Mathf.cos(buf2 + buf1);
-			float buf4 = P1.y + StoA * Mathf.sin(buf2 + buf1);
-			
+			buf2 = Mathf.Atan( (P2.y - P1.y) / (P1.x - P2.x) );
+			buf3 = P1.x - StoA * Mathf.Cos(buf2 - buf1);
+			buf4 = P1.y + StoA * Mathf.Sin(buf2 - buf1);
 		}
 		else{
-			float buf3 = P1.x + StoA * Mathf.cos(buf2 - buf1);
-			float buf4 = P1.y + StoA * Mathf.sin(buf2 - buf1);
+			buf2 = Mathf.Atan( (P2.y - P1.y) / (P2.x - P1.x) );
+			buf3 = P1.x + StoA * Mathf.Cos(buf2 - buf1);
+			buf4 = P1.y + StoA * Mathf.Sin(buf2 - buf1);
 		}
 		
-		Vector3 retPosition = new Vector3(buf3, buf4, 0f);
-		return retPosition;
-	}
-	
-	// 두 좌표의 중심점을 반환하는 함수
-	private Vector3 get_Middle_Pos(Vector3 P1, Vector3 P2){
+		if ( Length <= Mathf.Abs(AtoD-StoA) || Length >= AtoD+StoA ){
+			return PosAnkle_last;
+		}
 		
+		PosAnkle_last = new Vector3(buf3, buf4, 0f);
+		return PosAnkle_last;
 	}
 	
-	// 천장과 가까운 관절의 좌표를 반환하는 함수
-	public Vector3 get_Pos_AnkleA(bool Lefted){
-		PosAnkle = Get_Pos_Ankle(PosStart, PosDef, Lefted);
-		float X = PosAnkle.x - PosStart.x;
-		float Y = PosAnkle.y - PosStart.y;
-		X = X/2;
-		Y = Y/2;
-		Vector3 RET = new Vector3(PosStart.x + X, PosStart.y + Y, 0);
-		return RET;
+	/// <summary>
+	/// 천장과 가까운 관절의 좌표를 반환하는 함수
+	/// </summary>
+	private Vector3 get_Pos_AnkleA(){
+		PosAnkle = Get_Pos_Ankle(PosStart, PosDef);
+		return Get_Vector3_Middle(PosAnkle, PosStart);
 	}
 	
-	// 보스와 가까운 관절의 좌표를 반환하는 함수
-	public Vector3 get_Pos_AnkleB(bool Lefted){
-		PosAnkle = Get_Pos_Ankle(PosStart, PosDef, Lefted);
-		float X = PosDef.x - PosAnkle.x;
-		float Y = PosDef.y - PosAnkle.y;
-		X = X/2;
-		Y = Y/2;
-		Vector3 RET = new Vector3(PosAnkle.x + X, PosAnkle.y + Y, 0);
-		return RET;
+	/// <summary>
+	/// 보스와 가까운 관절의 좌표를 반환하는 함수
+	/// </summary>
+	private Vector3 get_Pos_AnkleB(){
+		PosAnkle = Get_Pos_Ankle(PosStart, PosDef);
+		return Get_Vector3_Middle(PosDef, PosAnkle);
 	}
 	
-	// 천장과 가까운 관절의 각도를 반환하는 함수
-	public Quaternion get_Quaternion_AnkleA(bool Lefted){
-		PosAnkle = Get_Pos_Ankle(PosStart, PosDef, Lefted);
+	/// <summary>
+	/// 천장과 가까운 관절의 각도를 반환하는 함수
+	/// </summary>
+	private Quaternion get_Quaternion_AnkleA(){
+		PosAnkle = Get_Pos_Ankle(PosStart, PosDef);
 		return Get_Pos_rotation(PosStart, PosAnkle);
 	}
 	
-	// 보스와 가까운 관절의 각도를 반환하는 함수
-	public Quaternion get_Quaternion_AnkleB(bool Lefted){
-		PosAnkle = Get_Pos_Ankle(PosStart, PosDef, Lefted);
+	/// <summary>
+	/// 보스와 가까운 관절의 각도를 반환하는 함수
+	/// </summary>
+	private Quaternion get_Quaternion_AnkleB(){
+		PosAnkle = Get_Pos_Ankle(PosStart, PosDef);
 		return Get_Pos_rotation(PosAnkle, PosDef);
 	}
+	
+	/// <summary>
+	/// 마우스의 위치를 받아오는 함수
+	/// </summary>
+	private void getMouseStaticPosition(){
+		MouseStaticPos = new Vector3(MainCamera.ScreenToWorldPoint(Input.mousePosition).x,MainCamera.ScreenToWorldPoint(Input.mousePosition).y,0);
+	}
+	
+	/// <summary>
+	/// PosStart와 PosDef에 관절 위치를 받아오는 함수, 함수 실행순서를 조정하기 위해 따로 선언함
+	/// </summary>
+	private void getPrivatePosition(){
+		PosStart = Root.position;
+		PosDef = MouseStaticPos;
+	}
+	
+	/// <summary>
+	/// 관절이 어느 방향으로 휘어야 하는지를 갱신하는 함수
+	/// </summary>
+	private void getLefted(){
+		if (PosStart.x >= PosDef.x) Lefted = false;
+		else Lefted = true;
+	}
+	
+	/// <summary>
+	/// 관절들의 위치와 각도를 조정하는 함수
+	/// </summary>
+	private void updateAnklePosition(){
+		Main.position = get_Pos_AnkleA();
+		Last.position = get_Pos_AnkleB();
+		Main.rotation = get_Quaternion_AnkleA();
+		Last.rotation = get_Quaternion_AnkleB();
+	}
+	
+	void Start(){
+		Root = GameObject.Find("RootAnkle").GetComponent<Transform>();
+		Main = GameObject.Find("MainAnkle").GetComponent<Transform>();
+		Last = GameObject.Find("LastAnkle").GetComponent<Transform>();
+	}
+	
+	void Update(){
+		getMouseStaticPosition();
+		getPrivatePosition();
+		getLefted();
+		updateAnklePosition();
+	}
+}
